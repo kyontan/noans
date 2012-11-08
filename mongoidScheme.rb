0703e99fd113829f0c2e878818280e0201095b40
Kyontan <kyontan@live.jp>
2012-11-09 00:07:27 +0900

    First commit! (なんで今までgit使わなかったん……
    11/7(Wed)  登録画面のデザイン完成(3日程度?)
    11/8(Thu) バリデーション, ひどいリダイレクト, process(completed)画面の作成
    11/9

---
+++ mongoidScheme.rb
@@ -0,0 +1,26 @@
+Mongoid.load!('mongoid.yml', :development)
+
+class User
+	include Mongoid::Document
+	include Mongoid::Timestamps
+	
+  field :name       ,:type => String
+  field :user       ,:type => String
+  field :password		,:type => String
+	
+  field :mail      	,:type => String
+	
+  #field :studentid  ,:type => Integer
+  field :admin      ,:type => Boolean, :default => false
+	
+	has_many :posts
+end
+
+class Post
+	include Mongoid::Document
+	include Mongoid::Timestamps
+	
+	field :text, type: String
+	
+	belongs_to :user
+end
