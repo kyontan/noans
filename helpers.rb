4183a4086299b4aaee71bf6738e059f70b4f8d42
Kyontan <kyontan@live.jp>
2013-03-20 21:08:56 +0900

    Disabled rack/csrf temporary Moved configs to configure from before Experimental added lib/sinatra/noans_helpers.rb

---
+++ helpers.rb
@@ -0,0 +1,11 @@
+#!/usr/local/bin/ruby
+
+require 'sinatra/base'
+
+module Sinatra
+	module NoansHelpers
+
+	end
+
+	helpers SomeHelpers
+end
--- views/less_compile.rb
+++ views/less_compile.rb
@@ -7,6 +7,8 @@ op = "-O2" unless ARGV.length.zero?
 
 Dir::glob(File.expand_path(File.dirname(__FILE__)) + "/*.less").each do |s|
 	f = s.match(/(.*?)\.less/)[1]
-	`lessc #{op} #{f}.less #{f}.css`
+	puts "Compiling #{f}.less"
+	`rm #{f}.css`
+	`lessc #{op} #{f}.less > #{f}.css`
 end
 `chown #{User}:#{Group} ./*`
