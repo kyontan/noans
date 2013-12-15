351b9cb02b23f4f23a8768593ffb4faf70056cf2
kyontan <kyontan@monora.me>
2013-12-15 22:21:56 +0900

    Modified dev-pages

--- routes/dev.rb
+++ routes/dev.rb
@@ -1,15 +1,11 @@
-# Coding:UTF-8
+# Coding: UTF-8
 
 before '/dev*' do
-	#require_login(true)
+	require_login(true)
 end
 
 get '/dev/?' do
-	haml :dev, locals: {common_css: true, title: "開発用", mode: "get"}
-end
-
-post '/dev/?' do
-	haml :dev, locals: {common_css: true, title: "開発用", mode: "post", params: params}
+	haml :dev, locals: {common_css: true, title: "開発用"}
 end
 
 get '/dev/raise/?' do
@@ -34,7 +30,7 @@ get '/dev/render/:obj/?' do
 			 	}
 end
 
-get '/dev/render2/:obj/?' do
+get '/dev/render_without_layout/:obj/?' do
 	haml params[:obj].to_sym, layout: false,
 				locals: {
 			 		path: params[:obj],
@@ -43,74 +39,12 @@ get '/dev/render2/:obj/?' do
 			 	}
 end
 
-get '/dev/user_data/:spec/?' do
-	eval "user_data.#{params[:spec]}"
-end
-
-# get '/dev/session/get/?' do
-# 	session[:dev]
-# end
-
-# get '/dev/session/set/?' do
-# 	session[:dev] = "Sessioned! #{Time.now}"
-# 	redirect "/session/get"
-# end
-
-# get '/dev/session/clear/?' do
-# 	session[:dev] = nil
-# end
-
-# get '/dev/post/?' do
-# 	"Post"
-# end
-
 get '/dev/raise/?' do
 	raise
 end
 
 get '/dev/reset/?' do
-	require_login(true)
-
 	system("touch ./tmp/restart.txt")
 
 	redirect back
-end
-
-get '/dev/md/:md/?' do
-	#markdown params[:md].to_sym
-
-	haml :markdown_dev, layout: false, locals: {md: params[:md], title: "dev render - #{params[:md]}.md"}
-end
-
-template :markdown_dev do
-<<'EOF'
-!!!5
-%html
-
-	%meta(charset = "utf-8")
-	%title Markdown preview - #{md}
-
-	:css
-		body {
-			width : 425px;
-			margin: 30px auto;
-		}
-
-		a { color:#888;	}
-
-		.response { display:none; }
-
-		.footer {
-			margin-top     : 20px;
-			padding-top    : 6px;
-			padding-bottom : 6px;
-			border-top     : -moz-linear-gradient(left,#fff,#ccc,#fff);
-			border-bottom  : -moz-linear-gradient(left,#fff,#ccc,#fff);
-			text-align     : center;
-			font-size      : 12px;
-			color          : #888;
-		}
-
-	= markdown md.to_sym
-EOF
 end
